8c1726345c805161f6700eedd6873ce09465ef8d
xianicai <zhanglibinit@163.com>
2017-05-03 22:38:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xianicai/recyclerviewcommonadapert/rvhelper/utils/BaseImageLoaderStrategy.java
@@ -0,0 +1,11 @@
+package com.example.xianicai.recyclerviewcommonadapert.rvhelper.utils;
+
+import android.content.Context;
+
+/**
+ * Created by Zhanglibin on 2017/5/3.
+ */
+
+public interface BaseImageLoaderStrategy {
+    void loadImage(Context ctx, ImageLoader img);
+}
